33e046239a8a96ca299cd7bf30eae1edcc3671d6
Wesley <data.wesleyunitedfa@gmail.com>
2025-08-27 03:15:24 +0200

    Update favicon to use actual FOLICEA logo from ImageKit

--- components/Favicon.tsx
+++ components/Favicon.tsx
@@ -10,7 +10,7 @@ const Favicon = () => {
       <link rel="icon" type="image/x-icon" href="/favicon.ico" />
       
       {/* Apple touch icon */}
-      <link rel="apple-touch-icon" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgwIiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDE4MCAxODAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHdpZHRoPSIxODAiIGhlaWdodD0iMTgwIiByeD0iMjAiIGZpbGw9IiMwMDI4NjgiLz4KPGNpcmNsZSBjeD0iOTAiIGN5PSI5MCIgcj0iNjAiIGZpbGw9IiNCRjBCMzAiLz4KPHRleHQgeD0iOTAiIHk9IjExMCIgZm9udC1mYW1pbHk9IkFyaWFsLCBzYW5zLXNlcmlmIiBmb250LXNpemU9IjQ4IiBmb250LXdlaWdodD0iYm9sZCIgZmlsbD0id2hpdGUiIHRleHQtYW5jaG9yPSJtaWRkbGUiPkY8L3RleHQ+Cjwvc3ZnPg==" />
+      <link rel="apple-touch-icon" href="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgwIiBoZWlnaHQ9IjE4MCIgdmlld0JveD0iMCAwIDE4MCAxODAiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxyZWN0IHdpZHRoPSIxODAiIGhlaWdodD0iMTgwIiByeD0iMjAiIGZpbGw9IndoaXRlIi8+CjxpbWFnZSBocmVmPSJodHRwczovL2lrLmltYWdla2l0LmlvL2ZvbGljZWFzdW1taXQvRk9MSUNFQSUyMFNVTU1JVC9Mb2dvLnBuZy5qcGc/dXBkYXRlZEF0PTE3NTYyMjcxNjI0MDMiIAp4PSIxMCIgeT0iMTAiIHdpZHRoPSIxNjAiIGhlaWdodD0iMTYwIiAKcHJlc2VydmVBc3BlY3RSYXRpbz0ieE1pZFlNaWQgbWVldCIvPgo8L3N2Zz4=" />
       
       {/* Theme color */}
       <meta name="theme-color" content="#BF0A30" />
